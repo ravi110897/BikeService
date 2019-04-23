01cc44d1cab93a20a3046837815753d9a38d2d1c
ravi110897 <47295349+ravi110897@users.noreply.github.com>
2019-04-23 10:03:40 +0530

    driver backbutton comment

--- app/src/main/java/com/example/ravi/bikeservice/DriverMapActivity.java
+++ app/src/main/java/com/example/ravi/bikeservice/DriverMapActivity.java
@@ -131,12 +131,12 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
 
     }
 
-    @Override
+    /*@Override
     public void onBackPressed() {
         Intent a = new Intent(Intent.ACTION_MAIN);
         a.addCategory(Intent.CATEGORY_HOME);
         a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(a);
-    }
+    }*/
 }
 
